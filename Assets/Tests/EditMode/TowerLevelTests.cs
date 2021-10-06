79c785a63f44cbb03430fe9ef80a76b796fb573d
Anatoly Koshelev <zeffyros451@gmail.com>
2021-10-06 14:32:04 +0300

    tests

---
+++ Assets/Tests/EditMode/TowerLevelTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TowerLevelTests
+    {
+        TowerLevel towerLevel;
+
+        public TowerLevelTests()
+        {
+            GameObject gameObject = new GameObject();
+            Tower tower = gameObject.AddComponent<Tower>();
+            towerLevel = new TowerLevel(tower, 1);
+        }
+
+        [Test]
+        public void ResetSetsLevelValueToZero()
+        {
+            towerLevel.Reset();
+            Assert.AreEqual(0, towerLevel.Value);
+        }
+    }
+}
