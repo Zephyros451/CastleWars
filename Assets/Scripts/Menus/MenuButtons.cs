216ba7aefd0decc89fbcfe9665a106fec4f7d12e
Anatoly Koshelev <zeffyros451@gmail.com>
2021-10-13 09:43:04 +0300

    new UI

---
+++ Assets/Scripts/Menus/MenuButtons.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MenuButtons : MonoBehaviour
+{
+    [SerializeField] private Canvas mainMenu;
+    [SerializeField] private Canvas levelsMenu;
+
+    private void Start()
+    {
+        ActivateMainMenu();
+    }
+
+    public void ActivateMainMenu()
+    {
+        levelsMenu.gameObject.SetActive(false);
+    }
+
+    public void ActivateLevelsMenu()
+    {
+        levelsMenu.gameObject.SetActive(true);
+    }
+}
